fcc89f29fe1565e59fa7466ae6626864a2c20647
ypavank <ypavank@gmail.com>
2018-01-09 13:38:48 -0600

    Reporting Solution

---
+++ BscReports/BscReportsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace BscReports
+{
+    public partial class BscReportsPage : ContentPage
+    {
+        public BscReportsPage()
+        {
+            InitializeComponent();
+
+            var grid = new Grid { ColumnSpacing = 5 };
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            var topLeft = new Label { Text = "Top Left" };
+            var topRight = new Label { Text = "Top Right" };
+            var bottomLeft = new Label { Text = "Bottom Left" };
+            var bottomRight = new Label { Text = "Bottom Right" };
+
+            grid.Children.Add(topLeft, 0, 0);
+            grid.Children.Add(topRight, 0, 1);
+            grid.Children.Add(bottomLeft, 1, 0);
+            grid.Children.Add(bottomRight, 1, 1);
+        }
+    }
+}
